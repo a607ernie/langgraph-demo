29aa5076a0926313bebb92d3f817e6de0b8f5b53
a607ernie <ernie60717@gmail.com>
2025-09-22 22:06:15 +0800

    新增圖形生成邏輯及狀態管理功能

---
+++ src/99.flowchat/run.py
@@ -0,0 +1,71 @@
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+
+from typing import Annotated
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph, END
+from langgraph.graph.message import add_messages
+
+from utils.graph2mermaid import create_mermaid # for saving mermaid code
+
+# 步驟 1：定義狀態
+class MyState(TypedDict):  # from typing import TypedDict
+    i: int
+    j: int
+    k: int
+    valid: str
+
+# Functions on **nodes**
+def parse(state: MyState):
+    print(f"parse: {state['i'], state['j'], state['k']}")
+    user_input = input("Enter three integers separated by spaces: ")
+    i, j, k = map(int, user_input.split())
+    return {"i": i, "j": j, "k": k}
+
+def validate(state: MyState):
+    i, j, k = state["i"], state["j"], state["k"]
+    
+    print(f"validate: {i}, {j}, {k}")
+    if i+j+k > 200:
+        return {"i": i, "j": j, "k": k, "valid": "TOO BIG"}
+
+    return {"i": i, "j": j, "k": k, "valid": "OK"}
+
+# Conditional **edge** function
+def is_big_enough(state: MyState):
+    if state['valid'] == "TOO BIG":
+        return END
+    else:
+        return "parse"
+
+# 步驟 4：構建圖
+def build_graph():
+    workflow = StateGraph(MyState)
+
+    workflow.add_node("parse", parse)
+    workflow.add_node("validate", validate)
+
+    workflow.set_entry_point("parse")
+
+    workflow.add_edge("parse", "validate")
+    workflow.add_conditional_edges(
+        source="validate", path=is_big_enough
+    )
+    graph = workflow.compile()
+    return graph
+
+
+
+if __name__ == "__main__":
+    # 運行聊天界面
+    
+    graph = build_graph()
+
+    # create_mermaid(graph)
+
+    user_input = {"i": 0, "j": 0, "k": 0}
+    r = graph.stream(user_input)
+    for item in r:
+        print(item)
+    
